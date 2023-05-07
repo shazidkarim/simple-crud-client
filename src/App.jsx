ae564691234beb37eb0f51fff98f93b83df80d1d
shazidkarim <shazid747@gmail.com>
2023-05-07 23:25:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const handleAddUser = event=>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user ={name,email};
+    console.log(user)
+    form.reset();
+
+    fetch('http://localhost:5000/users',{
+      method:'POST',
+      headers:{
+        'content-type':'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+  }
+
+  return (
+    <>
+     
+      <h1>simple CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="add user" />
+      </form>
+      
+    </>
+  )
+}
+
+export default App
